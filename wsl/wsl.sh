51e92c10b0dad673271e14780dab42593de3f68b
Alexej Harm <alexej.h@xiphos.de>
2021-02-01 10:30:16 +0100

    added markdown to html script

--- wsl/wsl.sh
+++ wsl/wsl.sh
@@ -5,3 +5,24 @@ if [ -x "/bin/wslpath" ] && [ -f "/mnt/c/Windows/System32/cmd.exe" ]; then
 fi
 
 alias crush="pngcrush -brute -reduce -rem allb -ow"
+
+md() {
+  if [ -z "$(which generate-md)" ]; then
+    echo "error: missing generate-md application"
+    echo ""
+    echo "installation instructions:"
+    echo ""
+    echo "  sudo npm install -g markdown-styles"
+    echo ""
+    exit 1
+  fi
+  if [ -z "${1}" ]; then
+    echo "error: missing input file"
+    exit 1
+  fi
+  if [ -z "${2}" ]; then
+    echo "error: missing output file"
+    exit 1
+  fi
+  generate-md --layout jasonm23-swiss --input "${1}" --output "${2}"
+}
